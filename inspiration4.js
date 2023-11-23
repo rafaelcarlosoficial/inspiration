cf6d59d59dd56ff78db0fdb2f5d3d246286a039b
rafaelcarlosoficial <143357132+rafaelcarlosoficial@users.noreply.github.com>
2023-11-23 11:31:17 -0300

    Projeto concluido
    só falta fazer a identação e excluir alguns comentários para o projeto ser concluído

--- inspiration4.js
+++ inspiration4.js
@@ -1,5 +1,4 @@
-//  let closeAnimation = false;
- let clickCounter = 0;
+
 //menu
 class MobileNavbar {
     constructor(mobileMenu, navList, navLinks) {
@@ -10,29 +9,31 @@ class MobileNavbar {
       this.disableClass = "disabled";
       this.handleClick = this.handleClick.bind(this);
     }
-    // directLinksOnMobile(){
-    //   this.
-    // }
+   
     closeMenu(){
-      this.navList.classList.remove(this.activeClass)
+      this.navList.classList.remove(this.activeClass);
       this.navList.classList.toggle(this.disableClass);
+      this.mobileMenu.classList.remove(this.activeClass);
+      
     }
 
     handleClick() {
       this.mobileMenu.classList.toggle(this.activeClass);
-      clickCounter++;
-      // this.navLinks.addEventListener('click', this.closeMenu);
-
-      if(clickCounter % 2) {
-        this.closeMenu()
-      } else {
+    
+      
+      if(this.mobileMenu.classList.contains(this.activeClass)) {
+        const menuLinks = document.querySelectorAll(".menuItem");
+        menuLinks.forEach(link => link.addEventListener("click", this.closeMenu.bind(this)));
         this.navList.classList.remove(this.disableClass);
         this.navList.classList.toggle(this.activeClass);
+      } else {
+        this.navList.classList.remove(this.activeClass)
+        this.navList.classList.toggle(this.disableClass);
       }
     }
 
+
     addClickEvent() {
-      clickCounter++;
       this.mobileMenu.addEventListener("click", this.handleClick);
     }
 
@@ -68,29 +69,25 @@ class MobileNavbar {
 const observer = new IntersectionObserver((entries) => {
 
   entries.forEach((entry) => {
-  console.log(entry);
   if(entry.isIntersecting){
-    entry.target.classList.add('show');
+    entry.target.classList.add("show");
   }
 })
 });
 
 
-  const hiddenElements = document.querySelectorAll('.hiddenLeft, .hiddenRight');
-  console.log(hiddenElements);
+  const hiddenElements = document.querySelectorAll(".hiddenLeft, .hiddenRight");
   hiddenElements.forEach((el) => observer.observe(el));
 
-  const heroSlider = document.querySelector('[data-hero-slider]');
-  const heroSliderItems = document.querySelectorAll('[data-hero-sliderItem]');
+  const heroSlider = document.querySelector("[data-hero-slider]");
+  const heroSliderItems = document.querySelectorAll("[data-hero-sliderItem]");
 
   let currentSlidePos = 0;
-  let lastActiveSliderItem = heroSliderItems[0]
+  let lastActiveSliderItem = heroSliderItems[0];
 
   const updateSliderpos = () => {
-    lastActiveSliderItem.classList.remove('active');
-    console.log("herosliderItems", heroSliderItems)
-    heroSliderItems[currentSlidePos].classList.add('active');
-    console.log("testando", heroSliderItems[currentSlidePos])
+    lastActiveSliderItem.classList.remove("active");
+    heroSliderItems[currentSlidePos].classList.add("active");
     lastActiveSliderItem = heroSliderItems[currentSlidePos];
   }
 
@@ -113,4 +110,32 @@ const observer = new IntersectionObserver((entries) => {
 
   }
   
-  window.addEventListener('load', autoSlide)
+  window.addEventListener("load", autoSlide);
+
+  // header e voltar ao topo
+  const header = document.querySelector("[data-header]");
+  const backTopBtn = document.querySelector("[data-back-top-btn]");
+  let lastScrollPos = 0;
+
+  const hideHeader = () => {
+    const isScrollBottom = lastScrollPos < window.scrollY
+
+    if(isScrollBottom) {
+      header.classList.add("hide");
+    } else {
+      header.classList.remove("hide");
+    }
+
+    lastScrollPos = window.scrollY;
+  }
+
+  window.addEventListener("scroll", () => {
+    if(window.scrollY >= 50){
+      header.classList.add("active");
+      backTopBtn.classList.add("active");
+    } else {
+      header.classList.remove("active");
+      backTopBtn.classList.remove("active");
+
+    }
+  })
